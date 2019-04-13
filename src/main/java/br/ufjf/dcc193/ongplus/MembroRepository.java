fc22812cbed771ef91550a9ba57f056c47a7868f
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-04-13 05:37:46 -0300

    Removendo bug

--- src/main/java/br/ufjf/dcc193/ongplus/MembroRepository.java
+++
@@ -1,9 +0,0 @@
-package br.ufjf.dcc193.ongplus;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MembroRepository extends JpaRepository<Membro, Long>{
-    List<Membro> findAll();
-}
