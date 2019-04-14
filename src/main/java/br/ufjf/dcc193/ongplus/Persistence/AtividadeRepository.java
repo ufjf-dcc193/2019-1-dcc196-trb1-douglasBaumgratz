a3d27c4e8d5387c2e2e7a80c2b8f4bb5d9b52110
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-04-14 19:02:36 -0300

    Atividade e Membro Repository

---
+++ src/main/java/br/ufjf/dcc193/ongplus/Persistence/AtividadeRepository.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.ongplus.Persistence;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufjf.dcc193.ongplus.Models.Atividade;
+
+
+
+public interface AtividadeRepository extends JpaRepository<Atividade, Long>{
+    List<Atividade> findAll();
+}
---
+++ src/main/java/br/ufjf/dcc193/ongplus/Persistence/MembroRepository.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.ongplus.Persistence;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufjf.dcc193.ongplus.Models.Membro;
+
+
+
+public interface MembroRepository extends JpaRepository<Membro, Long>{
+    List<Membro> findAll();
+}
