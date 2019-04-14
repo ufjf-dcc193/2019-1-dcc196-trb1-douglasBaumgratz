dec2b90a1deebffcc6f00bdab330569892249f42
douglasBaumgratz <douglas.baumgratz@gmail.com>
2019-04-14 18:22:47 -0300

    Criando pastas e organizando webapp/WEB-INF/views

--- src/main/java/br/ufjf/dcc193/ongplus/HomeController.java
+++ src/main/java/br/ufjf/dcc193/ongplus/HomeController.java
@@ -21,22 +21,22 @@ public class HomeController {
     @RequestMapping("sede.html")
     public String sede(Model model) {
         model.addAttribute("sede", sede.findAll());
-        return "sede";
+        return "sede/sede";
     }
 
     @RequestMapping("membro.html")
     public String membro(Model model) {
-        return "membro";
+        return "membro/membro";
     }
 
     @RequestMapping("atividade.html")
     public String atividade(Model model) {
-        return "atividade";
+        return "atividade/atividade";
     }
 
     @RequestMapping("relatorio.html")
     public String relatorio() {
-        return "relatorio";
+        return "relatorio/relatorio";
     }
 
     /*
@@ -44,19 +44,19 @@ public class HomeController {
      */
     @RequestMapping("sede_form.html")
     public String sede_form() {
-        return "sede_form";
+        return "sede/sede_form";
     }
 
     @RequestMapping("membro_form.html")
     public String membro_form(Model model) {
         model.addAttribute("sede", sede.findAll());
-        return "membro_form";
+        return "membro/membro_form";
     }
 
     @RequestMapping("atividade_form.html")
     public String atividade_form(Model model) {
         model.addAttribute("sede", sede.findAll());
-        return "atividade_form";
+        return "atividade/atividade_form";
     }
 
     /*
@@ -66,7 +66,7 @@ public class HomeController {
     public String cadastrar_sede(String nome_fantasia, String estado, String cidade, String bairro, int telefone,
             String endereco) {
         sede.save(new Sede(nome_fantasia, estado, cidade, bairro, telefone, endereco));
-        return "sede_form";
+        return "sede/sede_form";
     }
 
     /*
@@ -75,7 +75,7 @@ public class HomeController {
     @RequestMapping("excluir_sede.html")
     public String excluir_sede(Long id) {
         sede.deleteById(id);
-        return "sede_form";
+        return "sede/sede_form";
     }
 
 }
